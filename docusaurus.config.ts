957886ccae9f50e2eb9704d1b3f30f1efb851dc2
Jose A Guevara M <jguevara@grupomontecristo.com>
2026-02-09 08:27:55 -0600

    Se integran los cambios del repositorio

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://capifrank.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gm-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'capifrank', // Usually your GitHub org/user name.
+  projectName: 'gm-docs', // Usually your repo name.
+  deploymentBranch: 'build',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
@@ -103,7 +105,7 @@ const config: Config = {
           label: 'Docs',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/capifrank/gm-docs',
           label: 'GitHub',
           position: 'right',
         },
@@ -147,7 +149,7 @@ const config: Config = {
           items: [
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/capifrank/gm-docs',
             },
           ],
         },
